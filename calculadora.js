88e4097df4b8df7b6e8299c6b9a060710b29c6ac
ArthasPendragon007 <luizarthurbarros@hotmail.com>
2022-04-01 09:19:38 -0300

    1.0.1

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,18 @@
-var somar = require(".modulos/somar");
+var somar = require("./modulos/somar");
 
 console.log(somar(20,90));
+
+
+var subtrair = require("./modulos/subtrair");
+
+console.log(subtrair(20,90));
+
+
+var multiplicar = require("./modulos/multiplicar");
+
+console.log(multiplicar(20,90));
+
+
+var dividir = require("./modulos/dividir");
+
+console.log(dividir(20,90));
